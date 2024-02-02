393137c9d9aa576bc31cd52e17d9ebab807501d3
Jesus Legaspy <jesus.legaspy@fastmail.com>
2024-02-02 17:07:55 -0600

    Updated fonts

--- components/sections/Main.tsx
+++ components/sections/Main.tsx
@@ -1,3 +1,9 @@
+import { Montserrat } from 'next/font/google'
+import { Gidugu } from 'next/font/google'
+import { Gurajada } from 'next/font/google'
+import { Alegreya } from 'next/font/google'
+import { Roboto_Condensed } from 'next/font/google'
+import { Open_Sans } from 'next/font/google'
 import Image from 'next/image';
 import hero from '../../public/hero.jpg';
 import logo from '../../public/logo.png';
@@ -8,6 +14,10 @@ import { FaTruckPickup } from "react-icons/fa6";
 import { FaTruck } from "react-icons/fa6";
 import { FaCaravan } from "react-icons/fa6";
 import { FaTractor } from "react-icons/fa6";
+
+const montserrat = Montserrat({ subsets: ['latin'] });
+const gidugu = Gidugu({ subsets: ['latin'], weight: "400" });
+const gurajada = Gurajada({ subsets: ['latin'], weight: "400" });
 
 const Main = () => {
 
@@ -36,7 +46,7 @@ const Main = () => {
           </div>
 
         </div>
-        <div className='w-full bg-teal-950 text-white text-center uppercase '>
+        <div className={`w-full bg-teal-950 text-white text-center uppercase ${montserrat.className}`}>
           Hope's vehicle service experts
         </div>
       </div>
@@ -49,26 +59,26 @@ const Main = () => {
         />
         <div className='absolute inset-0'>
           <div className='flex flex-col justify-around items-center h-full'>
-            <div className='text-2xl text-white'>
+            <div className={`text-6xl text-white ${gidugu.className}`}>
               <div className='mr-8'>You have the wheels</div>
               <div className='text-right'>we have the skills</div>
             </div>
             <div className='grid grid-cols-3 gap-5 w-full md:w-5/8 lg:w-1/2 px-5 text-6xl sm:text-8xl'>
-              <div className='flex justify-center items-center w-full aspect-square bg-sky-50'><FaCar /></div>
-              <div className='flex justify-center items-center w-full aspect-square bg-sky-50'><FaMotorcycle /></div>
-              <div className='flex justify-center items-center w-full aspect-square bg-sky-50'><FaTruckPickup /></div>
-              <div className='flex justify-center items-center w-full aspect-square bg-sky-50'><FaTruck /></div>
-              <div className='flex justify-center items-center w-full aspect-square bg-sky-50'><FaCaravan /></div>
-              <div className='flex justify-center items-center w-full aspect-square bg-sky-50'><FaTractor /></div>
+              <div className='flex justify-center items-center w-full aspect-square bg-teal-50'><FaCar /></div>
+              <div className='flex justify-center items-center w-full aspect-square bg-teal-50'><FaMotorcycle /></div>
+              <div className='flex justify-center items-center w-full aspect-square bg-teal-50'><FaTruckPickup /></div>
+              <div className='flex justify-center items-center w-full aspect-square bg-teal-50'><FaTruck /></div>
+              <div className='flex justify-center items-center w-full aspect-square bg-teal-50'><FaCaravan /></div>
+              <div className='flex justify-center items-center w-full aspect-square bg-teal-50'><FaTractor /></div>
             </div>
-            <div className='bg-orange-600 text-white w-fit px-8 py-4 text-4xl rounded-2xl'>
+            <div className={`bg-orange-600 text-white w-fit px-8 py-2 text-6xl rounded-xl ${gurajada.className}`}>
               Appointment
             </div>
           </div>
         </div>
 
       </div>
-    </div>
+    </div >
   );
 }
 
