8d0465549eb83f92845e34d52dde629ebe4e82c2
Jesus Legaspy <jesus.legaspy@fastmail.com>
2024-02-14 12:17:37 -0600

    Styled reviews wip

--- components/sections/Reviews.tsx
+++ components/sections/Reviews.tsx
@@ -1,3 +1,6 @@
+import Image from 'next/image';
+import { BsThreeDotsVertical } from "react-icons/bs";
+import { IoStarSharp } from "react-icons/io5";
 
 const getReviews = async () => {
   const res = await fetch(`https://maps.googleapis.com/maps/api/place/details/json?fields=reviews&placeid=${process.env.GOOGLE_PLACE_ID}&key=${process.env.GOOGLE_API_KEY}`);
@@ -33,18 +36,61 @@ interface ResponseReview {
 const Reviews = async () => {
   const data: ResponseReview = await getReviews();
   const reviews = data.result.reviews;
+
+  const renderStars = (count: number) => {
+    let i = 0;
+    const stars = [];
+    for (i; i < count; i++) {
+      stars.push(
+        <IoStarSharp key={i} />
+      );
+    }
+    return stars;
+  }
+
   return (
-    <div className="max-w-screen-2xl w-full">
+    <div className="max-w-screen-2xl w-full mb-20">
       <div className='flex justify-center'>
         <div className='md:max-w-2xl p-4 mt-8'>
-          <div className="mb-4">
+          <div className="mb-6">
             <p>We believe the greatest compliment is a customer referral. That's why we provide all customers with the absolute best service and highest quality work, at fair and competitive prices. See what other have to say:</p>
           </div>
           <div>
             {reviews.map(review => (
-              <div key={review.time}>
-                <p>{review.author_name}</p>
-                <div>{review.text}</div>
+              <div key={review.time} className='text-sm border px-4 py-3 max-w-md'>
+                <div className="flex items-center w-fill justify-between mb-4">
+                  <div className='flex items-center '>
+                    <div>
+                      <Image
+                        src={review.profile_photo_url}
+                        width={24}
+                        height={24}
+                        alt="Picture of the reviewer"
+                      />
+                    </div>
+                    <div className='pl-2 font-bold'>
+                      {review.author_name}
+                    </div>
+                  </div>
+                  <div className='justify-self-end hover:bg-slate-200 rounded-full p-0.5'>
+                    <a href={review.author_url} target='_blank'>
+                      <BsThreeDotsVertical />
+                    </a>
+                  </div>
+                </div>
+                <div className='flex text-gray-600'>
+                  <div className='flex text-amber-500 mb-2'>
+                    {renderStars(review.rating)}
+                  </div>
+                  <div className='ml-2 text-xs'>
+                    {review.relative_time_description}
+                  </div>
+                </div>
+                <div>
+                  <p>
+                    {review.text}
+                  </p>
+                </div>
               </div>
             ))}
           </div>
