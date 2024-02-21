24bad0e89eccc4796bf01e6d545e7dbb06137d40
Jesus Legaspy <jesus.legaspy@fastmail.com>
2024-02-21 17:58:08 -0600

    Added google map image

--- components/sections/Maps.tsx
+++ components/sections/Maps.tsx
@@ -4,13 +4,16 @@ import Image from "next/image";
 const Maps = () => {
 
   return (
-    <div>
-      <Image
-        src={`https://maps.googleapis.com/maps/api/staticmap?center=San%20Francisco,CA&zoom=12&size=600x400&markers=san%20francisco,%20ca|37.811039,%20-122.477567&key=${process.env.GOOGLE_API_KEY}`}
-        alt={"Map of Green-go on Google Maps"}
-        width={500}
-        height={500}
-      />
+    <div className="flex  justify-center w-full bg-gray-200">
+      <div className="relative h-[600px] w-[800px]">
+        <Image
+          src={`https://maps.googleapis.com/maps/api/staticmap?center=33.6714934,-93.6083925&zoom=14&scale=2&size=800x600&markers=33.6648403,-93.6045737&key=${process.env.GOOGLE_API_KEY}`}
+          alt={"Map of Green-go on Google Maps"}
+          className="object-cover"
+          fill
+          priority
+        />
+      </div>
     </div>
   );
 };
