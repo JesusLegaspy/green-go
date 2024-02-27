85831b0a60793ca64d5f875a5427aa788de093bc
Jesus Legaspy <jesus.legaspy@fastmail.com>
2024-02-27 15:27:10 -0600

    Updated icons by clients request

--- components/components/Hero.tsx
+++ components/components/Hero.tsx
@@ -1,5 +1,9 @@
 import Image from 'next/image';
 import hero from '@/public/hero.jpg';
+import atvIcon from '@/public/atv-icon.png';
+import carIcon from '@/public/car-icon.png';
+import motorcycleIcon from '@/public/motorcycle-icon.png';
+import mufflerIcon from '@/public/muffler-icon.png';
 import { Gidugu } from 'next/font/google'
 import { Gurajada } from 'next/font/google'
 import { FaCar } from "react-icons/fa";
@@ -8,9 +12,11 @@ import { FaTruckPickup } from "react-icons/fa6";
 import { FaTruck } from "react-icons/fa6";
 import { FaCaravan } from "react-icons/fa6";
 import { FaTractor } from "react-icons/fa6";
+import { Shrikhand } from 'next/font/google';
 
 const gidugu = Gidugu({ subsets: ['latin'], weight: "400" });
 const gurajada = Gurajada({ subsets: ['latin'], weight: "400" });
+const shrikhand = Shrikhand({ subsets: ['latin'], weight: "400" });
 
 
 const Hero = () => {
@@ -30,12 +36,44 @@ const Hero = () => {
             <div className='text-right'>we have the skills</div>
           </div>
           <div className='grid grid-cols-3 gap-5 w-full sm:w-3/4 md:w-3/5 lg:w-4/12 xl:w-3/12 px-5 text-6xl sm:text-7xl'>
-            <div className='flex justify-center items-center w-full aspect-square bg-senary '><FaCar /></div>
-            <div className='flex justify-center items-center w-full aspect-square bg-senary '><FaMotorcycle /></div>
-            <div className='flex justify-center items-center w-full aspect-square bg-senary '><FaTruckPickup /></div>
-            <div className='flex justify-center items-center w-full aspect-square bg-senary '><FaTruck /></div>
-            <div className='flex justify-center items-center w-full aspect-square bg-senary '><FaCaravan /></div>
-            <div className='flex justify-center items-center w-full aspect-square bg-senary '><FaTractor /></div>
+            <div className='relative flex justify-center items-center w-full aspect-square bg-senary rounded-2xl'>
+              <Image
+                src={carIcon}
+                alt={'Car icon'}
+                className='object-contain p-3'
+                fill
+              />
+            </div>
+            <div className='relative flex justify-center items-center w-full aspect-square bg-senary rounded-2xl'>
+              <Image
+                src={mufflerIcon}
+                alt={'Car icon'}
+                className='object-contain p-3'
+                fill
+              />
+            </div>
+            <div className='relative flex justify-center items-center w-full aspect-square bg-senary rounded-2xl'>
+              <Image
+                src={atvIcon}
+                alt={'Car icon'}
+                className='object-contain p-3'
+                fill
+              />
+            </div>
+            <div className='relative flex justify-center items-center w-full aspect-square bg-senary rounded-2xl'>
+              <Image
+                src={motorcycleIcon}
+                alt={'Car icon'}
+                className='object-contain p-3'
+                fill
+              />
+            </div>
+            <div className='relative flex justify-center items-center w-full aspect-square bg-senary rounded-2xl'>
+              <FaCaravan />
+            </div>
+            <div className='relative flex justify-center items-center w-full aspect-square bg-senary rounded-2xl'>
+              <FaTractor />
+            </div>
           </div>
           <a href="tel:8708260950">
             <div className={`bg-orange-600 text-white w-fit px-8 py-2 text-6xl rounded-xl sm:mb-6 ${gurajada.className}`}>
