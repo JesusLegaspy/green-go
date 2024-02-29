1501153917ec04a9a4944e7d3e97eed4cdd54d8c
Jesus Legaspy <jesus.legaspy@fastmail.com>
2024-02-29 16:44:10 -0600

    Top Bar

--- components/components/TopBar.tsx
+++ components/components/TopBar.tsx
@@ -4,19 +4,34 @@ import React, { useState, useEffect } from 'react';
 import { FaMapMarker } from "react-icons/fa";
 import { Roboto_Condensed } from 'next/font/google';
 import { FaPhone } from "react-icons/fa6";
+import logoMd from "@/public/gglogo_md.webp";
+import Image from "next/image";
+import { Alegreya } from 'next/font/google';
 
-
+const alegreya = Alegreya({ subsets: ['latin'], weight: "400" });
 const robotoCondensed = Roboto_Condensed({ subsets: ["latin"] });
 
 const TopBar = () => {
-  const [scrollPosition, setScrollPosition] = useState<number>(0);
+  const [topPosition, setTopPostition] = useState<string>('-top-28');
   const handleScroll = () => {
-    if (window.scrollY > 50 && scrollPosition === 0) {
-      setScrollPosition(100);
-    } else {
-      setScrollPosition(0);
+    if (768 < window.screen.width) {
+      if (75 < window.scrollY) {
+        setTopPostition('-top-12');
+        return;
+      }
+      setTopPostition('-top-28');
+      return;
     }
-
+    if (75 < window.scrollY && window.scrollY < 200) {
+      setTopPostition('-top-12');
+      return;
+    }
+    if (200 < window.scrollY) {
+      setTopPostition('top-0');
+      return;
+    }
+    setTopPostition('-top-28');
+    return;
   };
 
   useEffect(() => {
@@ -28,8 +43,8 @@ const TopBar = () => {
   }, []);
 
   return (
-    <div className="lg:hidden w-full">
-      <div className={`z-40 fixed flex justify-between items-center text-xl font-bold bg-primary py-3 px-2 w-full transition-[top] ${scrollPosition < 50 ? 'top-[-50px]' : 'top-0'} ${robotoCondensed.className}`}>
+    <div className={`z-40 fixed w-full transition-[top] ${topPosition} flex flex-col shadow-md`}>
+      <div className={`bg-primary py-3 px-2 flex justify-between items-center text-xl font-bold ${robotoCondensed.className} `}>
         <div className='flex items-center'>
           <FaMapMarker />
           <div className='text-xs'>
@@ -39,6 +54,24 @@ const TopBar = () => {
         <div className='flex items-center'>
           <FaPhone />
           <div className='text-sm ml-2'>870-826-0950</div>
+        </div>
+      </div>
+      <div className='flex bg-cyan-900 p-2'>
+        <div className='flex items-center w-full'>
+          <div className='bg-primary p-0.5 rounded-full mt-0.5'>
+            <Image
+              alt='Green-go logo medium'
+              src={logoMd}
+              className='object-cover'
+              height={64}
+              width={64}
+            />
+          </div>
+          <a href='tel:8708260950'>
+            <div className={`flex bg-secondary py-1 px-3 ${alegreya.className} text-white rounded text-lg ml-6 mt-0.5`}>
+              Appointment Now <FaPhone className='ml-2 text-xs self-center' />
+            </div>
+          </a>
         </div>
       </div>
     </div>
